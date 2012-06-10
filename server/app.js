0d5b7d82568cb3afe19f56e2488f42bbdaa44daa
Don Smith <locksmithdon@gmail.com>
2012-06-11 00:28:25 +1200

    updated for working on azure and fixed it to write filenames properly on ec2

--- server/app.js
+++ server/app.js
@@ -1,27 +1,38 @@
 var formidable = require('formidable'),
     http = require('http'),
     util = require('util'),
-    fs   = require('fs');
+    fs   = require('fs'),
+    port = process.env.port || 1337; // ;)
 
 http.createServer(function(req, res) {
   
   console.log(req.url);
 
   if(req.url == '/upload' && req.method.toLowerCase() == 'post') {
-    // parse a file upload
     var form = new formidable.IncomingForm();
     form.keepExtensions = true;
     form.uploadDir = '/srv/nodejs/apps/we/media';
-    form.parse(req, function(err, fields, files) {
-      res.writeHead(200, {'content-type': 'text/plain'});
-      res.write('received upload:\n\n');
-      res.end(util.inspect({fields: fields, files: files}));
-    });
+    form
+      .on('fileBegin', function(name, file) {
+              file.path = form.uploadDir + '/' + file.name;
+      })
+      .on('end', function() {
+            res.writeHead(200, {'content-type': 'text/plain'});
+            res.end('received files');
+      });
+    form.parse(req);
+  }
+
+  if(req.url == '/upload-success' && req.method.toLowerCase() == 'get') {
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.write('received upload:\n\n');
+    res.end('<h1>Sweet as!</h1>')
   }
 
   if(req.url == '/' && req.method.toLowerCase() == 'get') {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<!DOCTYPE html><html><head><title>We-estimate</title></head><body>');
+    res.write('<h1>We-estimate</h1><h3>Uploaded videos</h3><ol>');
 
     //var files = fs.readdirSync(process.cwd()+'/media');
     //for(var i=0; i < files.length; i++) {
@@ -30,7 +41,7 @@ http.createServer(function(req, res) {
 
     res.write('<em>This will be replaced by a list of estimate requests</em>')
 
-    res.end('</body></html>');
+    res.end('</ol><p><em>&copy; 2012 We-estimate</em></p></body></html>');
   }
 
   if(req.url.substr(-4, 4).toLowerCase() == '.mov') {
@@ -47,6 +58,4 @@ http.createServer(function(req, res) {
     });
   }
 
-}).listen(8004, '0.0.0.0');
-
-console.log('The we-estimate site is now listening on port 8004');
+}).listen(port, '0.0.0.0');
