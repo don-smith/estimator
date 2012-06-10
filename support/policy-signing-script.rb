47dffeacd6303b9a7046f23d21e076a05a3f6929
Don Smith <locksmithdon@gmail.com>
2012-06-11 00:27:36 +1200

    adjust signing policy files to keep them up to date

--- support/policy-signing-script.rb
+++ support/policy-signing-script.rb
@@ -5,14 +5,15 @@ require 'digest/sha1'
 policy_document = '{ "expiration": "2013-01-01T00:00:00Z",
   "conditions": [ 
     {"bucket": "we-estimate-videos"}, 
-    ["starts-with", "$key", "upload/"],
+    ["starts-with", "$key", "uploads/"],
     {"acl": "public-read"},
-    {"success_action_redirect": "http://we.locksmithdon.net/upload-success"},
+    {"success_action_redirect": "http://estimator.azurewebsites.net/upload-success"},
     ["starts-with", "$Content-Type", "video"],
     ["content-length-range", 0, 4194304]
   ]
 }'
 
+aws_access_key = "1Z9R29H6R69CHFGVJ202" # not used in this script
 aws_secret_key = "y6pgb7yl+W6V2+iwlVnmOQimj8cGF1qcNNLfJcUU"
 
 policy = Base64.encode64(policy_document).gsub("\n","")
